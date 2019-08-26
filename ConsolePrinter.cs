75d492863dadbf8031b14254da20545bd99246a1
Joanna Cholewinska <cholewinskaj@gmail.com>
2019-08-26 19:06:22 +0200

    eh

--- ConsolePrinter.cs
+++ ConsolePrinter.cs
@@ -9,12 +9,10 @@ namespace Nowe_zadanie
     {
         public void Printer()
         {
-            ToDoApp toDoApp = new ToDoApp();
-            List<Task> _taskList;
-            _taskList = new List<Task>();
             FileSaver fileSaver = new FileSaver();
         
             string path = @"C:\Users\hermiodek\Desktop\MojaLista.txt";
+            
             string readText = File.ReadAllText(path, Encoding.UTF8);
             Console.WriteLine(readText);
         }
